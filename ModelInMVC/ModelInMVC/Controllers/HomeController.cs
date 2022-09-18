7c81add434b4d0b426814864a78611b4a633b852
Ashutosh kumar <kumarashutosh5499@gmail.com>
2022-09-18 20:32:35 +0530

    Models In MVC.
    Models In MVC.

---
+++ ModelInMVC/ModelInMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelInMVC.Models;
+
+namespace ModelInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = GetEmployee();
+            return View(data);
+        }
+
+
+        //  GetEmployee is a method which is used to get data and its return type is employee type
+        // and this method is used to return the data.
+
+
+        private Employee GetEmployee()
+        {
+            return new Employee()
+            {
+
+                Id = 1,
+                Address = "India",
+                Name = "WebGentle"
+
+            };
+        }
+
+
+    }
+}
---
+++ ModelInMVC/ModelInMVC/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelInMVC.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    
+}
