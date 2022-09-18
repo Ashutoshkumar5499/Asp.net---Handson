b124f284f9408b12289ac21750b4b77d7983a2e7
Ashutosh kumar <kumarashutosh5499@gmail.com>
2022-09-18 19:11:14 +0530

    In this handson I have learnt Controller and View.
    In this handson I have learnt Controller and View.

---
+++ Assignment_1st/Assignment_1st/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_1st.Controllers
+{
+    public class PagesController : Controller
+    {
+       
+        public  ActionResult Idex()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment_1st/Assignment_1st/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment_1st.MvcApplication" Language="C#" %>
